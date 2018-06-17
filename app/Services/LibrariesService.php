1af0d519f205fab7c4e44c83bb8a06bc56252b9c
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-17 20:29:34 +0300

    structure set up

---
+++ app/Services/LibrariesService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\ServiceAbstract;
+
+class LibrariesService extends ServiceAbstract
+{
+  public function get()
+  {
+    
+  }
+}
---
+++ app/Transformers/LibrariesTransformer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Transformers\TransformerAbstract;
+
+class LibrariesTransformer extends TransformerAbstract
+{
+  public function transform()
+  {
+    
+  }
+}
