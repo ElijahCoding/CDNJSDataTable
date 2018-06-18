0b58df0cdcc734463608e688137358fb30c3f884
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-18 15:58:41 +0300

    styling and fetching data

--- resources/assets/js/components/DataTable.vue
+++ resources/assets/js/components/DataTable.vue
@@ -3,13 +3,25 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
                     <div class="card-body">
 
                       <div class="table-responsive">
                         <table class="table table-stripe">
-                          
+                          <thead>
+                            <tr>
+                              <th>Name</th>
+                              <th>Link</th>
+
+                              <th>&nbsp;</th>
+                              <th>&nbsp;</th>
+                            </tr>
+                          </thead>
+
+                          <tbody>
+                            <tr>
+
+                            </tr>
+                          </tbody>
                         </table>
                       </div>
 
@@ -28,12 +40,53 @@
 
         data () {
           return {
-
+            response: {
+              name: '',
+              link: [],
+            },
           }
         },
 
         mounted() {
-            console.log(this.endpoint)
+            axios.get(this.endpoint).then((response) => {
+              this.response = response.data
+              console.log(this.response);
+            })
         }
     }
 </script>
+
+<style lang="scss">
+    .sortable {
+        cursor: pointer;
+    }
+
+    .match {
+        color: #4fc08d;
+    }
+
+    .well {
+        border-radius: 0;
+    }
+
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin-left: 5px;
+        opacity: 0.66;
+
+        &--asc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-bottom: 4px solid #222;
+        }
+
+        &--desc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-top: 4px solid #222;
+        }
+    }
+</style>
